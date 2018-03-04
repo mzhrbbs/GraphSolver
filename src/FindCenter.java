816940ad412bffe1311b560b156ef7bdd02d7a99
mazhar_abbass <mzhrbbs@gmail.com>
2018-03-03 19:43:24 -0800

    Update FindCenter.java

--- src/FindCenter.java
+++ src/FindCenter.java
@@ -40,9 +40,8 @@ public class FindCenter extends HttpServlet {
 
 		graph = (int[][]) session.getAttribute("graph");
 		Object json = session.getAttribute("json");
-
+		
 		int[] priArray = (int[]) session.getAttribute("priorityArray");
-
 		List<Integer> result = getPaths(graph);
 
 		session.setAttribute("dist", result);
@@ -57,82 +56,72 @@ public class FindCenter extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 
 	}
 
 	public static void print(int[][] dist) {
-
+		
 		int V = dist.length;
 		for (int i = 0; i < V; ++i) {
 
 			for (int j = 0; j < V; ++j) {
-				if (dist[i][j] == 9999)
-					;
-				// System.out.print("INF ");
-				else
-					;
-
-				// System.out.print(dist[i][j] + " ");
+				
+				if (dist[i][j] == 9999);
 			}
-			// System.out.println();
 		}
 	}
-
+	
+	// Dynamic programming approach
 	List<Integer> getPaths(int[][] graph) {
+		
 		int V = graph.length;
-
 		int dist[][] = new int[V][V];
 
 		// Base case
 		for (int i = 0; i < V; i++) {
+			
 			for (int j = 0; j < V; j++) {
+				
 				dist[i][j] = graph[i][j];
 			}
 		}
-
 		for (int k = 0; k < V; k++) {
 
 			for (int i = 0; i < V; i++) {
 
 				for (int j = 0; j < V; j++) {
 
-					if (dist[i][k] + dist[k][j] < dist[i][j])
-						dist[i][j] = dist[i][k] + dist[k][j];
+					if (dist[i][k] + dist[k][j] < dist[i][j]) dist[i][j] = dist[i][k] + dist[k][j];
 				}
-
 			}
 		}
 		return getCenters(dist);
 	}
-
+	
 	List<Integer> getCenters(int dist[][]) {
+		
 		int V = dist.length;
-
 		int[] len = new int[V];
+		
 		for (int i = 0; i < V; i++) {
+			
 			len[i] = 0;
 			for (int j = 0; j < V; j++) {
-				if (i != j && dist[i][j] > len[i])
-					len[i] = dist[i][j];
+				if (i != j && dist[i][j] > len[i]) len[i] = dist[i][j];
 			}
 		}
-
 		int center = len[0];
 		for (int i = 1; i < len.length; i++) {
-			if (len[i] <= center)
-				center = len[i];
+			
+			if (len[i] <= center) center = len[i];
 		}
-
-		// Get the centers in array
 		ArrayList<Integer> res = new ArrayList<>();
+		
 		for (int i = 0; i < len.length; i++) {
-			if (len[i] == center)
-				res.add(i);
+			
+			if (len[i] == center) res.add(i);
 		}
-
 		return res;
 	}
-
 }
